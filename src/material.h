4228142a312319c2d8059e9d434eae75b80d2ec6
Varad Sharma <varad.sharma@uwaterloo.ca>
2021-04-14 19:57:01 -0400

    feat: add fuzziness to metals

--- src/material.h
+++ src/material.h
@@ -37,12 +37,13 @@ class lambertian : public material {
 
 class metal : public material {
     public:
-        metal(const color& a) : albedo(a) {}
+        metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
         virtual bool scatter(
             const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
         ) const override {
             vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+            scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
             scattered = ray(rec.p, reflected);
             attenuation = albedo;
             return (dot(scattered.direction(), rec.normal) > 0);
@@ -50,5 +51,6 @@ class metal : public material {
 
     public:
         color albedo;
+        double fuzz;
 };
 #endif
